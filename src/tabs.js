fd3d38f4f7ded9ff7836ee9bcfc1a72b78b855a7
graemebyrne_89@hotmail.com <graemebyrne_89@hotmail.com>
2022-02-20 01:49:54 +0000

    added javascipt to help display info on destination page

--- src/tabs.js
+++
@@ -1,7 +0,0 @@
-const tabList = document.querySelector('[role="tablist"]')
-const tabs = tabList.querySelectorAll('[role="tab"]')
-
-tabList.addEventListener('keydown', (e) => {
-    const keydownLeft = 37
-    const keydownRight = 39
-})
